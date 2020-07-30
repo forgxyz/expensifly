dc26148d06f980f93ca8c5e230a63c171856dab6
forgxyz <jmfxyz@pm.me>
2020-07-30 13:07:22 -0400

    model setup

--- expensifly/record/models.py
+++ expensifly/record/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+    # this won't change or have user input... different type?
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category
+
+
+class Method(models.Model):
+    # categorical type?
+    method = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.method
+
+
+class Expense(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    date = models.DateField(default=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    method = models.ForeignKey(Method, on_delete=models.DO_NOTHING)
+    comment = models.CharField(max_length=100)
+    tag = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.comment} for {self.amount} on {self.date}"
--- expensifly/record/urls.py
+++ expensifly/record/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+# application namespace
+app_name = 'record'
+
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+
+# in template: {% url 'record:index' %}
